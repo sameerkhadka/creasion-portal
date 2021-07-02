9c0a319860cf79fde99781153a2a5374e7cfc3d7
Kshitiz Shrestha <er.kshitizshrestha@gmail.com>
2021-07-02 13:28:14 +0545

    changes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\District;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChartController;
+use App\Municipality;
+use App\Province;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +19,27 @@ use App\Http\Controllers\ChartController;
 
 // Route::get('/p', function () {
 //     return view('p');
+// });
+
+// Route::get('importData',function(){
+//     $provinces = json_decode(file_get_contents(public_path().'/json/provinces.json'),true)['provinces'];
+//     $districts = json_decode(file_get_contents(public_path().'/json/districts.json'),true)['districts'];
+//     $municipalities = json_decode(file_get_contents(public_path().'/json/municipalities.json'),true)['municipalities'];
+//     foreach($municipalities as $municipality){
+//         Municipality::create(
+//             [
+//                 "title" => $municipality['title'],
+//                 "title_en" => $municipality['title_en'],
+//                 "title_ne" => $municipality['title_ne'],
+//                 "code" => $municipality['code'],
+//                 "type" => $municipality['type'],
+//                 "order" => $municipality['order'],
+//                 "district_id" => $municipality['district'],
+//                 "centroid" => json_encode($municipality['centroid']),
+//                 "bbox" => json_encode($municipality['bbox']),
+//             ]
+//         );
+//     }
 // });
 
 Route::get('/', [ChartController::class,'index'])->name('index');
