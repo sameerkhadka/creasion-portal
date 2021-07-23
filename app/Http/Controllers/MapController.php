e5fcfc3c6f83157d1d5a7225dfc00ac1668194bd
sameerkhadka <sameerkhadka3@gmail.com>
2021-07-23 15:48:45 +0545

    Partner added in CMS

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Project;
+use App\Models\Partner;
 use Illuminate\Http\Request;
 
 class MapController extends Controller
 {
     public function index(){
         $projects = Project::all();
-        return view('map.index',compact('projects'));
+        $partners = Partner::all();
+        return view('map.index',compact('projects','partners'));
     }
 }
