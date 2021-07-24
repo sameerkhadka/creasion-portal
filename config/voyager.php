514da8319acfdae2d02a3073ed2fe470e52b2302
Kshitiz Shrestha <er.kshitizshrestha@gmail.com>
2021-07-25 00:00:42 +0545

    admin panel some changes

--- config/voyager.php
+++ config/voyager.php
@@ -187,11 +187,11 @@ return [
 
     // Here you can specify additional assets you would like to be included in the master.blade
     'additional_css' => [
-        //'css/custom.css',
+        'css/voyager.css',
     ],
 
     'additional_js' => [
-        //'js/custom.js',
+        'js/voyager.js',
     ],
 
     'googlemaps' => [
