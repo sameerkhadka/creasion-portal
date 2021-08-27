4436b538eec27b7b4583082338d5b30c9ae8357f
yogesh-karki <yogesh@hueshine.com>
2021-08-27 16:46:16 +0545

    ofn chapters

--- resources/views/ofn-chapters.blade.php
+++ resources/views/ofn-chapters.blade.php
@@ -4,11 +4,37 @@
             <div class="container">
                 <div class="faq-head">
                     <h4>OFN Chapters</h4>
-                    <p>Nepal Relief is an initiative of CREASION for quick disaster response.
-                    hrough Nepal Relief, we focus on quick response as a short- term solution 
-                    and move towards rehabilitation as a long-term solution to disaster management. 
+                    <p>OFN has been raising funds and procuring necessary equipment to address
+                         the needy patients as well as front liners. However, Nepal needs more 
+                         of the support to address the ongoing overwhelmed situation. In order 
+                         to address the needs of people all over Nepal, we are initiating to reach 
+                         out to all the seven provinces. The second chapter will be able to 
+                         decentralize the COVID-19 response support of Oxygen For Nepal by 
+                         dispatching response resources to all the provinces of the country
+                          whereas the first chapter focused on providing relief to the capital
+                           city, Kathmandu. We target to achieve following points: 
                     </p>
+
+                    <div class="ofn-chapter">
+                        <ul class="ofn-chap-list">
+                            <li>Provide COVID-19 response support (oxygen cylinders, concentrators, PPE, food drive and other safety) to the hot spots of Nepal.  </li>
+                            <li>Provide COVID-19 relief support to remote areas of Nepal.</li>
+                            <li>Build a network of communities/organizations willing to provide support to the needy ones in collaboration with Oxygen For Nepal for optimum reach. </li>
+                            <li>Support local organizations or groups that are actively working for crisis management by providing them necessary resources. </li>
+                        </ul>
+
+                        <p>
+                            OFN Chapter II has begun in Chitwan and Rautahat and various safety resources as well as cylinders are provided to the districts so that there is decentralized facilitation. Until today, OFN has been able to reach Kathmandu, Lalitpur, Bhaktapur, Jumla, Banke, Kavre, Kapilvastu, Chitwan, Dhading, Sindhupalchowk, Parsa, Rautahat, Ramechhap, Sindhuli, Siraha, Saptari, Jhapa, Khotang, Bhojpur, Sankhuwasabha, Lamjung, Humla, Darchula, Manang, Tanahun, Gorkha, Kalikot  
+                        </p>
+
+                    </div>
+                    
                 </div>
+
+                
+
+
+                
 
               
             </div>
